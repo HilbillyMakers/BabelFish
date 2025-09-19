f9140be12fa13c8ed053684952283dd2ee22d448
MadBerry58 <maxim.mura@proton.me>
2025-09-19 03:54:45 +0300

    Copied basic TinyUSB usb/device/dev_lowlevel example into project

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
+#include "usb_interface/usb_lowlevel.h"
+
 #define LED_DELAY_MS 1000u
 
 
@@ -11,16 +13,19 @@ int main()
 {
     timer_hw->dbgpause = 0;
     stdio_init_all();
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    printf("USB Device Low-Level hardware example\n");
+    usb_device_init();
 
-    while(1)
-    {
-        gpio_put(PICO_DEFAULT_LED_PIN, true);
-        sleep_ms(LED_DELAY_MS);
-        printf("Test Message 0\n");
-        gpio_put(PICO_DEFAULT_LED_PIN, false);
-        sleep_ms(LED_DELAY_MS);
-        printf("Test Message 1\n");
+    // Wait until configured
+    while (!configured) {
+        tight_loop_contents();
+    }
+
+    // Get ready to rx from host
+    usb_start_transfer(usb_get_endpoint_configuration(EP1_OUT_ADDR), NULL, 64);
+
+    // Everything is interrupt driven so just loop here
+    while (1) {
+        tight_loop_contents();
     }
 }
