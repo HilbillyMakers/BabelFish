71947b6d4c7bd27b84192e67f75e13a3dd2cdd14
Luminush <luminitacaplea@gmail.com>
2026-03-02 20:18:43 +0200

    Update Cmake paths and calculate 32 bit baudrate

--- modules/spi/spiHandlers.c
+++ modules/spi/spiHandlers.c
@@ -6,19 +6,34 @@
 
 ///TODO: 
 /* 
-    Prototype SPI pins are: GP18 - GP21 (pinout SPI0)
+    Prototype SPI pins are: GP10 - GP13 (pinout SPI0)
     Future implementation: user is allowed to customize pin usage,
     need to check for conflicts and coherence
  */  
 
+//default pins for SPI peripheral
 #define PROTOTYPE_SPI       spi0
-#define SPI_SCK_PIN         18 
+#define SPI_SCK_PIN         10 
+#define SPI_TX_PIN          11 
+#define SPI_RX_PIN          12
+#define SPI_CSN_PIN         13
+
+//comment above and uncomment these when testing CAN communication through SPI
+/*
+#define PROTOTYPE_SPI       spi0
+#define SPI_SCK_PIN         18
 #define SPI_TX_PIN          19 
 #define SPI_RX_PIN          20
 #define SPI_CSN_PIN         21
+*/
 
+///TODO: 
+/*
+    This functionality can be common to multiple protocols
+    Should move in a common generic header file
+*/
 #define SLAVE_SELECTED      0
-#define SLAVE_NOT_SELECTED  1
+#define SLAVE_UNSELECTED    1
 
 enum
 {
@@ -53,4 +68,56 @@ uint8_t com_spi_commandStringHandler (const uint8_t *commandString,  uint8_t com
     }
 
     return responseSize;
+}
+
+uint8_t com_spi_init        (uint8_t* const configuration, uint8_t* const responseBuffer, uint8_t* const errorBuffer)
+{
+
+    uint32_t baudRate               =   0u;
+    uint32_t physicalBaudRate       =   0u;
+    
+    // Reassemble our uint32_t baudRate from 4 uint8_t variables
+    baudRate                        =   configuration[0]        |
+                                       (configuration[1]) <<  8 |
+                                       (configuration[2]) << 16 |
+                                       (configuration[3]) << 24;
+
+    physicalBaudRate                =   spi_init(PROTOTYPE_SPI, baudRate);
+    *((uint32_t *)responseBuffer)   =   physicalBaudRate;
+
+    gpio_set_function   (SPI_RX_PIN,    GPIO_FUNC_SPI);
+    gpio_set_function   (SPI_TX_PIN,    GPIO_FUNC_SPI);
+    gpio_set_function   (SPI_SCK_PIN,   GPIO_FUNC_SPI);
+
+    // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_init           (SPI_CSN_PIN);
+    gpio_set_dir        (SPI_CSN_PIN,   GPIO_OUT);
+    gpio_put            (SPI_CSN_PIN,   SLAVE_UNSELECTED);
+
+    return sizeof(physicalBaudRate);
+}
+
+uint8_t com_spi_deinit      (uint8_t* const responseBuffer, uint8_t* const errorBuffer)
+{
+    spi_deinit(PROTOTYPE_SPI);
+
+    return 0;
+}
+
+uint8_t com_spi_transfer    (uint8_t* const hostMessage,    uint8_t* const responseBuffer, uint8_t* const errorBuffer)
+{
+    uint8_t     txSize          =   hostMessage[0];
+    uint8_t     rxSize          =   hostMessage[1];
+    uint8_t    *txMessage       =  &hostMessage[2];
+    int8_t      receivedBytes   =   0u;     
+    //chip select
+    gpio_put            (SPI_CSN_PIN,   SLAVE_SELECTED);
+    //send request to slave
+    spi_write_blocking  (PROTOTYPE_SPI, txMessage, txSize);
+    //receive data from slave
+    receivedBytes   = spi_read_blocking(PROTOTYPE_SPI, 0, responseBuffer, rxSize);
+    //chip deselect
+    gpio_put            (SPI_CSN_PIN,   SLAVE_UNSELECTED);
+
+    return receivedBytes;
 }
